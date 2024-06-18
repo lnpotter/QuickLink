79ed361f6b3d501b78e1d635eae8ba7116a0b37d
LordLucass <lucasnoalpottergithub@gmail.com>
2024-06-18 12:50:25 -0300

    feat(test/post.ps1): adding test help

--- tests/post.ps1
+++ tests/post.ps1
@@ -7,3 +7,7 @@ $body = @{
 $response = Invoke-RestMethod -Uri $url -Method Post -ContentType "application/json" -Body ($body | ConvertTo-Json)
 
 $response | Format-Table -AutoSize
+
+# If you get an error about the execution policy, you may need to change the PowerShell execution policy to allow scripts to run.
+# You can do this with the command:
+# Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
